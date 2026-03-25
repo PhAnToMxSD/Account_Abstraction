de0d94a404c52423608ae7b27ae249cac3dbb459
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-25 18:52:51 +0530

    DeployMent Script ready

--- script/DeployMinimal.s.sol
+++ script/DeployMinimal.s.sol
@@ -3,10 +3,19 @@ pragma solidity 0.8.30;
 
 import {Script} from "forge-std/Script.sol";
 import {MinimalAccount} from "src/MinimalAccount.sol";
+import {CONFIG} from "./HelperConfig.s.sol";
 
 contract DeployMinimal is Script {
     function run() external {
+    }
+    function deployMinimalAccount() external returns (CONFIG, MinimalAccount){
+        CONFIG fig = new CONFIG();
+        CONFIG.Config memory config = fig.getConfigByChainID();
+
         vm.startBroadcast();
+        MinimalAccount minimal = new MinimalAccount(config.entryPoint);
+        minimal.transferOwnership(msg.sender);
         vm.stopBroadcast();
+        return (fig, minimal);
     }
 }
